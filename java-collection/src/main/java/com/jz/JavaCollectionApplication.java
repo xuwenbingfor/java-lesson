11a951c5f72d67a75819c9d635f7a7a769ee038e
xuwenbingfor <1965104943@qq.com>
2020-05-03 10:42:55 +0800

    冲突处理

---
+++ java-collection/src/main/java/com/jz/JavaCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.jz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCollectionApplication.class, args);
+    }
+
+}
---
+++ java-lang/src/test/java/com/jz/JavaLangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
