c2ab0d7142bcdc5ba0f981fb1f96cd34a38bfad8
xuwenbingfor <1965104943@qq.com>
2020-06-14 21:46:05 +0800

    jvm options test

---
+++ java-jvm/src/test/java/com/jz/jvm/MainTests.java
@@ -0,0 +1,16 @@
+package com.jz.jvm;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author xuwenbingfor
+ * @version 2020/6/14 9:13
+ * @description
+ */
+public class MainTests {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+}
